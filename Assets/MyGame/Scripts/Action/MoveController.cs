fe4193cac1eb3e7c4af1c92d71429d8d2b10b0bf
sorude26 <sorude26@gmail.com>
2022-07-08 14:20:18 +0900

    移動関連の処理を追加した

--- Assets/MyGame/Scripts/Action/MoveController.cs
+++ Assets/MyGame/Scripts/Action/MoveController.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class MoveController : MonoBehaviour
+/// <summary>
+/// à⁄ìÆÇëÄçÏÇ∑ÇÈ
+/// </summary>
+public class MoveController
 {
-    // Start is called before the first frame update
-    void Start()
+    private const float BRAKE_DECELERATE = 0.3f;
+    private Rigidbody _rb = default;
+    public MoveController(Rigidbody rigidbody)
     {
-        
+        _rb = rigidbody;
     }
-
-    // Update is called once per frame
-    void Update()
+    public void VelocityMove(Vector3 dir)
     {
-        
+        _rb.velocity = dir;
+    }
+    public void AddMove(Vector3 dir)
+    {
+        _rb.AddForce(dir,ForceMode.Force);
+    }
+    public void AddImpulse(Vector3 dir)
+    {
+        _rb.AddForce(dir, ForceMode.Impulse);
+    }
+    public void MoveBreak(float breakPower = BRAKE_DECELERATE)
+    {
+        var velocity = _rb.velocity;
+        float y  = velocity.y;
+        velocity *= breakPower;
+        velocity.y = y;
+        _rb.velocity = velocity;
     }
 }
