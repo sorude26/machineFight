57a4e9c630891c93657de7a2b90fc7341fbdc503
sorude26 <sorude26@gmail.com>
2022-07-08 20:39:57 +0900

    ステート操作処理を追加した

--- Assets/MyGame/Scripts/Machine/StateController.cs
+++ Assets/MyGame/Scripts/Machine/StateController.cs
@@ -8,18 +8,22 @@ namespace MyGame.MachineFrame
     {
         private MoveController _moveController = default;
         private AnimatorController _animatorController = default;
+        private IMachineState _currentState = default;
+        private StateIdle _idle = default;
         public StateController(MoveController moveController,AnimatorController animatorController)
         {
             _moveController = moveController;
             _animatorController = animatorController;
+            _idle = new StateIdle();
+            _currentState = _idle;
         }
         public void Update()
         {
-
+            _currentState.OnUpdate(this);
         }
         public void FixedUpdate()
         {
-
+            _currentState.OnFixedUpdate(this);
         }
     }
 }
