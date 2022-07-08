2abcf96929dc11721e9a52910de5ab1948b3a68f
sorude26 <sorude26@gmail.com>
2022-07-08 20:22:11 +0900

    namespaceを設定した

--- Assets/MyGame/Scripts/Action/MoveController.cs
+++ Assets/MyGame/Scripts/Action/MoveController.cs
@@ -1,35 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/// <summary>
-/// à⁄ìÆÇëÄçÏÇ∑ÇÈ
-/// </summary>
-public class MoveController
+
+namespace MyGame
 {
-    private const float BRAKE_DECELERATE = 0.3f;
-    private Rigidbody _rb = default;
-    public MoveController(Rigidbody rigidbody)
+    /// <summary>
+    /// à⁄ìÆÇëÄçÏÇ∑ÇÈ
+    /// </summary>
+    public class MoveController
     {
-        _rb = rigidbody;
-    }
-    public void VelocityMove(Vector3 dir)
-    {
-        _rb.velocity = dir;
-    }
-    public void AddMove(Vector3 dir)
-    {
-        _rb.AddForce(dir,ForceMode.Force);
-    }
-    public void AddImpulse(Vector3 dir)
-    {
-        _rb.AddForce(dir, ForceMode.Impulse);
-    }
-    public void MoveBreak(float breakPower = BRAKE_DECELERATE)
-    {
-        var velocity = _rb.velocity;
-        float y  = velocity.y;
-        velocity *= breakPower;
-        velocity.y = y;
-        _rb.velocity = velocity;
+        private const float BRAKE_DECELERATE = 0.3f;
+        private Rigidbody _rb = default;
+        public MoveController(Rigidbody rigidbody)
+        {
+            _rb = rigidbody;
+        }
+        public void VelocityMove(Vector3 dir)
+        {
+            _rb.velocity = dir;
+        }
+        public void AddMove(Vector3 dir)
+        {
+            _rb.AddForce(dir, ForceMode.Force);
+        }
+        public void AddImpulse(Vector3 dir)
+        {
+            _rb.AddForce(dir, ForceMode.Impulse);
+        }
+        public void MoveBreak(float breakPower = BRAKE_DECELERATE)
+        {
+            var velocity = _rb.velocity;
+            float y = velocity.y;
+            velocity *= breakPower;
+            velocity.y = y;
+            _rb.velocity = velocity;
+        }
     }
 }
