4151fe70cc00e8da70228511eb1f44bd63193ff3
sorude26 <sorude26@gmail.com>
2022-07-08 20:41:08 +0900

    マシンの操作用スクリプトを追加した

---
+++ Assets/MyGame/Scripts/Machine/MachineController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyGame.MachineFrame
+{
+    public class MachineController : MonoBehaviour
+    {
+        private void FixedUpdate()
+        {
+            
+        }
+    }
+}
