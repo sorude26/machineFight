8bd133b6685d929ba64134add49b716bd81ffc8b
sorude26 <sorude26@gmail.com>
2022-07-09 19:46:37 +0900

    ステートの変更機能と各状態の処理を追加した

--- Assets/MyGame/Scripts/Machine/StateController.cs
+++ Assets/MyGame/Scripts/Machine/StateController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,72 @@ namespace MyGame.MachineFrame
 {
     public partial class StateController
     {
-        private MoveController _moveController = default;
-        private AnimatorController _animatorController = default;
-        private IMachineState _currentState = default;
-        private StateIdle _idle = default;
-        public StateController(MoveController moveController,AnimatorController animatorController)
+        protected MoveController _moveController = default;
+        protected AnimatorController _animatorController = default;
+        protected IMachineState _currentState = default;
+        protected StateType _currentStateType = default;
+        protected StateIdle _stateIdle = new StateIdle();
+        protected StateWalk _stateWalk = new StateWalk();
+        protected StateFall _stateFall = new StateFall();
+        protected StateJump _stateJump = new StateJump();
+        protected StateFloat _stateFloat = new StateFloat();
+        protected StateLanding _stateLanding = new StateLanding();
+        protected Dictionary<StateType, string> _stateAnimationNames = new Dictionary<StateType, string>();
+
+        public event Func<bool> OnChackGround = default;
+        public StateController(MoveController moveController, AnimatorController animatorController)
         {
             _moveController = moveController;
             _animatorController = animatorController;
-            _idle = new StateIdle();
-            _currentState = _idle;
+            _currentState = _stateIdle;
+        }
+        protected void ChangeAnimation(StateType type)
+        {
+            if (!_stateAnimationNames.ContainsKey(type)) { return; }
+            _animatorController.ChangeAnimation(_stateAnimationNames[type]);
+        }
+        protected void ChangeState(StateType type)
+        {
+            if (_currentStateType == type) { return; }
+            switch (type)
+            {
+                case StateType.Idle:
+                    _currentState = _stateIdle;
+                    break;
+                case StateType.Walk:
+                    _currentState = _stateWalk;
+                    break;
+                case StateType.Jump:
+                    _currentState = _stateJump;
+                    break;
+                case StateType.Fall:
+                    _currentState = _stateFall;
+                    break;
+                case StateType.Landing:
+                    _currentState = _stateLanding;
+                    break;
+                case StateType.Float:
+                    _currentState = _stateFloat;
+                    break;
+                default:
+                    break;
+            }
+            _currentState.OnEnter(this);
+        }
+        protected void ChackFallOnGround()
+        {
+            _moveController.MoveBreak();
+            if (!(bool)OnChackGround?.Invoke())
+            {
+                ChangeState(StateType.Fall);
+            }
+        }
+        protected void ChackGround()
+        {
+            if ((bool)OnChackGround?.Invoke())
+            {
+                ChangeState(StateType.Landing);
+            }
         }
         public void Update()
         {
