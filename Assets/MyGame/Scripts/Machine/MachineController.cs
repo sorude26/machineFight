41f8048770070a998c6c37a774497252394424eb
sorude26 <sorude26@gmail.com>
2022-07-09 20:32:38 +0900

    コントローラーの処理を追加した

--- Assets/MyGame/Scripts/Machine/MachineController.cs
+++ Assets/MyGame/Scripts/Machine/MachineController.cs
@@ -6,9 +6,43 @@ namespace MyGame.MachineFrame
 {
     public class MachineController : MonoBehaviour
     {
+        [SerializeField]
+        float _moveSpeed = 10f;
+        [SerializeField]
+        private WallChecker _groundChecker = default;
+        [SerializeField]
+        private Rigidbody _rigidbody = default;
+        [SerializeField]
+        private AnimatorController _animatorController = default;
+        private MoveController _moveController = default;
+        private StateController _stateController = default;
+        private void Start()
+        {
+            _moveController = new MoveController(_rigidbody);
+            _animatorController.OnMove += WalkMove;
+            _animatorController.OnJump += JumpMove;
+            _animatorController.OnStop += MoveBreak;
+            _stateController = new StateController(_moveController, _animatorController, _groundChecker);
+        }
         private void FixedUpdate()
         {
-            
+            _stateController.FixedUpdate();
+        }
+        private void WalkMove()
+        {
+            _moveController.AddMove(transform.forward * _moveSpeed);
+        }
+        private void JumpMove()
+        {
+            _moveController.AddImpulse(transform.forward + Vector3.up * _moveSpeed);
+        }
+        private void MoveBreak()
+        {
+            _moveController.MoveBreak();
+        }
+        private bool IsGround()
+        {
+            return _groundChecker.IsWalled();
         }
     }
 }
