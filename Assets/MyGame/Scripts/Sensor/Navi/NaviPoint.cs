6f4424e8b6c441ac3d5589460de3904b29821546
sorude26 <sorude26@gmail.com>
2022-07-15 18:55:16 +0900

    クラス名の変更を行った

--- Assets/MyGame/Scripts/Sensor/Navi/NaviPoint.cs
+++ Assets/MyGame/Scripts/Sensor/Navi/NaviPoint.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NaviStagePoint:IMapPoint<NaviStagePoint>
+public class NaviPoint:IMapPoint<NaviPoint>
 {
     public Vector3 Pos { get; }
     public int IndexID { get; }
     public SearchStateType State { get; set; }
-    public List<NaviStagePoint> ConnectPoint { get; set; } = new List<NaviStagePoint>();
+    public List<NaviPoint> ConnectPoint { get; set; } = new List<NaviPoint>();
     public int Cost { get; set; }
     public int DistanceCost { get; set; }
     public int TotalCost { get; set; }
     public int Footprints { get; set; }
-    public NaviStagePoint Parent { get; set; }
-    public NaviStagePoint(Vector3 pos, int id)
+    public NaviPoint Parent { get; set; }
+    public NaviPoint(Vector3 pos, int id)
     {
         Pos = pos;
         IndexID = id;
