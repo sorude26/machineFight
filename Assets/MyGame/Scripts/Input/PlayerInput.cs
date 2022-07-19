477aa4f6f102cf3ae7b87c201a210ca6810b7a39
sorude26 <sorude26@gmail.com>
2022-07-19 19:03:33 +0900

    プレイヤーの入力管理用スクリプトを追加した

---
+++ Assets/MyGame/Scripts/Input/PlayerInput.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace MyGame
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        private static PlayerInput instance;
+        private Vector2 _moveDir = default;
+        private InputControls _controls = default;
+        /// <summary> “ü—Í’¼Œã </summary>
+        private Dictionary<InputType, Action> _onEnterInputDic = new Dictionary<InputType, Action>();
+        /// <summary> “ü—Í’† </summary>
+        private Dictionary<InputType, Action> _onStayInputDic = new Dictionary<InputType, Action>();
+        /// <summary> “ü—Í‰ğœ </summary>
+        private Dictionary<InputType, Action> _onExitInputDic = new Dictionary<InputType, Action>();
+        public static Vector2 MoveDir 
+        {
+            get 
+            {
+                if (instance is null)
+                {
+                    Initialization();
+                }
+                return instance._moveDir;
+            }
+        }
+        public static PlayerInput Instance
+        {
+            get 
+            {
+                if (instance is null)
+                {
+                    Initialization();
+                }
+                return instance; 
+            }
+        }
+        private static void Initialization()
+        {
+            var obj = new GameObject("PlayerInput");
+            instance = obj.AddComponent<PlayerInput>();
+            instance._controls = new InputControls();
+            instance._controls.Enable();
+            instance.InitializationInput();
+            instance._controls.InputMap.Move.performed += context => { instance._moveDir = context.ReadValue<Vector2>(); };
+            instance._controls.InputMap.Move.canceled += context => { instance._moveDir = Vector2.zero; }; 
+            instance._controls.InputMap.Jump.started += context => { instance._onEnterInputDic[InputType.Jump]!.Invoke(); };
+            instance._controls.InputMap.Jump.canceled += context => { instance._onExitInputDic[InputType.Jump]!.Invoke(); };
+        }
+        private void InitializationInput()
+        {
+            for (int i = 0; i < Enum.GetValues(typeof(InputType)).Length; i++)
+            {
+                _onEnterInputDic.Add((InputType)i, () => { });
+                _onStayInputDic.Add((InputType)i, () => { });
+                _onExitInputDic.Add((InputType)i, () => { });
+            }
+        }
+        public static void SetEnterInput(InputType type, Action action)
+        {
+            Instance._onEnterInputDic[type] += action;
+        }
+        public static void LiftEnterInput(InputType type, Action action)
+        {
+            instance._onEnterInputDic[type] -= action;
+        }
+    }
+    public enum InputType
+    {
+        /// <summary> Œˆ’è“ü—Í </summary>
+        Submit,
+        /// <summary> ƒLƒƒƒ“ƒZƒ‹“ü—Í </summary>
+        Cancel,
+        /// <summary> ƒWƒƒƒ“ƒv“ü—Í </summary>
+        Jump,
+        /// <summary> UŒ‚“ü—Í‚P </summary>
+        Fire1,
+        /// <summary> UŒ‚“ü—Í‚Q </summary>
+        Fire2,
+        /// <summary> UŒ‚“ü—Í‚R </summary>
+        Fire3,
+    }
+}
