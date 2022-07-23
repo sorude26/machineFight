5829447bc9b289724cae71723bc6487145acb7d0
sorude26 <sorude26@gmail.com>
2022-07-23 15:03:07 +0900

    銃のクラスを追加した

---
+++ Assets/MyGame/Scripts/Weapon/ShotWeapon.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyGame.MachineFrame
+{
+    public class ShotWeapon : WeaponBase
+    {
+        public override void Fire()
+        {
+        }
+    }
+}
