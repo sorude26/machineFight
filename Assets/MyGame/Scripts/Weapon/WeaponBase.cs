97725c4e3b2483247a186ac261cca3ae9079cbc0
sorude26 <sorude26@gmail.com>
2022-07-23 15:02:39 +0900

    武装の基底クラスを追加した

---
+++ Assets/MyGame/Scripts/Weapon/WeaponBase.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyGame.MachineFrame
+{
+    public abstract class WeaponBase : MonoBehaviour
+    {
+        public abstract void Fire();
+    }
+}
