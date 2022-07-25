2d60f34b39a8455cd3e157a30929f66a9b86b781
sorude26 <sorude26@gmail.com>
2022-07-25 18:44:22 +0900

    武器と弾丸に発射処理を追加した

--- Assets/MyGame/Scripts/Weapon/Bullet.cs
+++ Assets/MyGame/Scripts/Weapon/Bullet.cs
@@ -3,8 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace MyGame.MachineFrame
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class Bullet : MonoBehaviour
     {
-        
+        [SerializeField]
+        private Rigidbody _rb = default;
+        [SerializeField]
+        private float _lifeTime = 1f;
+        private float _timer = 0f;
+        private void OnEnable()
+        {
+            _timer = 0;
+        }
+        private void Update()
+        {
+            _timer += Time.deltaTime;
+            if (_timer > _lifeTime)
+            {
+                _timer = 0;
+                _rb.velocity = Vector3.zero;
+                gameObject.SetActive(false);
+            }
+        }
+        public void Shot(Vector3 dir)
+        {
+            transform.forward = dir;
+            _rb.AddForce(dir, ForceMode.Impulse);
+        }
     }
 }
--- Assets/MyGame/Scripts/Weapon/ShotWeapon.cs
+++ Assets/MyGame/Scripts/Weapon/ShotWeapon.cs
@@ -6,8 +6,17 @@ namespace MyGame.MachineFrame
 {
     public class ShotWeapon : WeaponBase
     {
+        [SerializeField]
+        private Transform _muzzle = default;
+        [SerializeField]
+        private float _shotSpeed = 100;
+        [SerializeField]
+        private Bullet _bulletPrefab = default;
         public override void Fire()
         {
+            var bullet = BulletPool.GetObject(_bulletPrefab);
+            bullet.transform.position = _muzzle.position;
+            bullet.Shot(_muzzle.forward * _shotSpeed);
         }
     }
 }
