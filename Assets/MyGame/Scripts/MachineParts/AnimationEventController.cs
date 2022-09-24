08f449cbbebef57a897599a88e3504b1f1222929
sorude26 <sorude26@gmail.com>
2022-09-24 15:08:31 +0900

    アニメーションイベント用スクリプトを追加した

---
+++ Assets/MyGame/Scripts/MachineParts/AnimationEventController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+/// <summary>
+/// アニメーションイベントで呼ばれる機能を持つ
+/// </summary>
+public class AnimationEventController : MonoBehaviour
+{
+    [SerializeField]
+    private ShakeParam _shakeParam = default;
+    [SerializeField]
+    private ShakeParam _strongParam = default;
+    private void PlayShake()
+    {
+        StageShakeController.PlayShake(transform.position + _shakeParam.Pos, _shakeParam.Power, _shakeParam.Time);
+    }
+    private void PlayStrongShake()
+    {
+        StageShakeController.PlayShake(transform.position + _strongParam.Pos, _strongParam.Power, _strongParam.Time);
+    }
+}
