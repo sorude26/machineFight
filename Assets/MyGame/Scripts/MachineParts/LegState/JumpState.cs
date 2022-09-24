15b053dd03578416518ac9adec20bc4001982c9c
sorude26 <sorude26@gmail.com>
2022-09-24 17:05:01 +0900

    ジャンプステートを追加した

---
+++ Assets/MyGame/Scripts/MachineParts/LegState/JumpState.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public partial class LegStateContext
+{
+    protected class JumpState : ILegState
+    {
+        public void ExecuteEnter(LegStateContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ExecuteExit(LegStateContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ExecuteUpdate(LegStateContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
