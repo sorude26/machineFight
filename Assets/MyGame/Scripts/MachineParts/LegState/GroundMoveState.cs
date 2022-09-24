41b20ba0435cfa42f0e8be25abb798caaca0a061
sorude26 <sorude26@gmail.com>
2022-09-24 19:35:59 +0900

    視点に対しての方向指示で移動するように処理を修正した

--- Assets/MyGame/Scripts/MachineParts/LegState/GroundMoveState.cs
+++ Assets/MyGame/Scripts/MachineParts/LegState/GroundMoveState.cs
@@ -15,6 +15,7 @@ public partial class LegStateContext
             Right,
         }
         private LegAngle _currentAngle;
+        private Quaternion _legR = Quaternion.identity;        
         private void LegMove(LegStateContext context)
         {
             if (context._moveDir == Vector3.zero)
@@ -28,26 +29,31 @@ public partial class LegStateContext
                 return;
             }
             Vector3 dir = context._moveDir.normalized;
-            Vector3 lockDir = dir;
+            Quaternion lockQ = Quaternion.Euler(0, (90f - Mathf.Abs(dir.z) * 45f) * dir.x, 0);
+            Vector3 moveDir = context.LegTrans.forward;
             if (dir.z < 0)
             {
-                lockDir *= -1;
+                lockQ = Quaternion.Euler(0, (90f - Mathf.Abs(dir.z) * 45f) * -dir.x, 0);
                 if (_currentAngle != LegAngle.Back)
                 {
                     _currentAngle = LegAngle.Back;
                     context.ChangeAnimetion(context._animeName.Back);
                 }
-            } else if (dir == -context.LegTransform.forward)
-            {
-                lockDir = context.LegTransform.forward + context.BodyTransform.forward;
+                moveDir = -moveDir;
             }
-            context.LegTransform.forward = Vector3.Lerp(context.LegTransform.forward, lockDir,context._actionParam.WalkTurnSpeed * Time.fixedDeltaTime);
-            if (Vector3.Dot(lockDir, context.LegTransform.forward) < context._actionParam.DotSub)
+            if (lockQ == Quaternion.Euler(0, -_legR.x, 0))
+            {
+                lockQ = Quaternion.identity;
+            }
+            float local = Mathf.Abs(context.LegTrans.localRotation.y);
+            float target = Mathf.Abs(lockQ.y);
+            context.LegTrans.localRotation = Quaternion.Lerp(context.LegTrans.localRotation, lockQ, context._actionParam.WalkTurnSpeed * Time.fixedDeltaTime);
+            if (Mathf.Max(local, target) - Mathf.Min(local, target) > context._actionParam.DotSub)
             {
                 if (_currentAngle != LegAngle.Back)
                 {
-                    var angle = Vector3.Cross(context.LegTransform.forward, context.BodyTransform.forward);
-                    if (angle.y < 0)
+                    float angleY = context.LegTrans.localRotation.y - lockQ.y;
+                    if (angleY > 0)
                     {
                         if (_currentAngle != LegAngle.Left)
                         {
@@ -55,7 +61,7 @@ public partial class LegStateContext
                             context.ChangeAnimetion(context._animeName.TurnLeft);
                         }
                     }
-                    else if (angle.y > 0)
+                    else if (angleY < 0)
                     {
                         if (_currentAngle != LegAngle.Right)
                         {
@@ -63,7 +69,8 @@ public partial class LegStateContext
                             context.ChangeAnimetion(context._animeName.TurnRight);
                         }
                     }
-                    dir = Vector3.zero;
+                    context._moveController.VelocityMove(Vector3.zero);
+                    return;
                 }
             }
             else if (_currentAngle != LegAngle.Front && dir.z >= 0)
@@ -71,16 +78,16 @@ public partial class LegStateContext
                 _currentAngle = LegAngle.Front;
                 context.ChangeAnimetion(context._animeName.Walk);
             }
-            context._moveController.VelocityMove(dir * context._actionParam.WalkSpeed);
+            context._moveController.VelocityMove(moveDir * context._actionParam.WalkSpeed);
         }
-        public void ExecuteEnter(LegStateContext context) 
+        public void ExecuteEnter(LegStateContext context)
         {
             _currentAngle = LegAngle.Idle;
         }
 
-        public void ExecuteExit(LegStateContext context) {}
+        public void ExecuteExit(LegStateContext context) { }
 
-        public void ExecuteUpdate(LegStateContext context) 
+        public void ExecuteUpdate(LegStateContext context)
         {
             if (context._jumpInput == true)
             {
