6495243964a9eb022a3f98f915e4c4f389d40ec0
sorude26 <sorude26@gmail.com>
2022-09-24 17:06:06 +0900

    パラメータ設定用構造体を追加した

--- Assets/MyGame/Scripts/MachineParts/LegState/ILegState.cs
+++ Assets/MyGame/Scripts/MachineParts/LegState/ILegState.cs
@@ -1,10 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ILegState
+using System;
+public partial class LegStateContext
 {
-    void ExecuteEnter(LegStateContext context);
-    void ExecuteUpdate(LegStateContext context);
-    void ExecuteExit(LegStateContext context);
+    public interface ILegState
+    {
+        void ExecuteEnter(LegStateContext context);
+        void ExecuteUpdate(LegStateContext context);
+        void ExecuteExit(LegStateContext context);
+    }
+}
+[Serializable]
+public struct LegAnimation 
+{
+    public string Idle;
+    public string Walk;
+    public string Back;
+    public string TurnLeft;
+    public string TurnRight;
+    public string Jump;
+    public string Landing;
+}
+[Serializable]
+public struct LegActionParam
+{
+    public float WalkSpeed;
+    public float WalkTurnSpeed;
+    public float JumpPower;
+    public float DotSub;
+    public float AnimeChangeSpeed;
 }
