8b46a121c07c582753bdabb4ec08202580df0596
sorude26 <sorude26@gmail.com>
2022-09-24 17:08:01 +0900

    脚部ステートマシーンの変数、処理を追加した

--- Assets/MyGame/Scripts/MachineParts/LegState/LegStateContext.cs
+++ Assets/MyGame/Scripts/MachineParts/LegState/LegStateContext.cs
@@ -2,18 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LegStateContext
+public partial class LegStateContext
 {
     private ILegState _currentState = default;
+    private Animator _legAnimator = default;
+
+    private Vector3 _moveDir = default;
+    private bool _jumpInput = false;
+    private bool _groundCheck = false;
+    private MyGame.MoveController _moveController = default;
+
+    private GroundMoveState _groundState = new GroundMoveState();
+    private JumpState _jumpState = new JumpState();
+
+    public LegAnimation _animeName = default;
+    public LegActionParam _actionParam = default;
     public Transform LegTransform = default;
     public Transform BodyTransform = default;
-    public LegStateContext()
+    public LegStateContext(Animator animator, MyGame.MoveController moveController)
     {
+        _legAnimator = animator;
+        _moveController = moveController;
+        _currentState = _groundState;
         _currentState.ExecuteEnter(this);
     }
 
-    public void ExecuteUpdate(Vector3 moveDir,bool groundCheck)
+    public void ExecuteUpdate(Vector3 moveDir, bool jumpInput, bool groundCheck)
     {
+        _moveDir = moveDir;
+        _jumpInput = jumpInput;
+        _groundCheck = groundCheck;
         _currentState.ExecuteUpdate(this);
     }
     public void ChangeState(ILegState legState)
@@ -21,5 +39,9 @@ public class LegStateContext
         _currentState.ExecuteExit(this);
         _currentState = legState;
         _currentState.ExecuteEnter(this);
+    }
+    private void ChangeAnimetion(string target)
+    {
+        _legAnimator.CrossFadeInFixedTime(target, _actionParam.AnimeChangeSpeed);
     }
 }
