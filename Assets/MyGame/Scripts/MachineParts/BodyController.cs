1f624af84f171182f9c38185361184812d5f006b
sorude26 <sorude26@gmail.com>
2022-09-24 19:36:51 +0900

    脚部パーツ操作処理を追加した

--- Assets/MyGame/Scripts/MachineParts/BodyController.cs
+++ Assets/MyGame/Scripts/MachineParts/BodyController.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class BodyController : MonoBehaviour
 {
+    [SerializeField]
+    private float _rotationSpeed = 0.5f;
     public Transform BodyJoint = null;
     public Transform LockRoute = null;
+    public Transform LegBase = null;
+    public Transform LegTrans = null;
+    private Quaternion _bodyRotaion;
     private void FixedUpdate()
     {
+        _bodyRotaion = Quaternion.Euler(0, MyGame.PlayerInput.CameraDir.x * 90f, 0);
         transform.position = BodyJoint.position;
-        //transform.localRotation = LockRoute.localRotation;
+        LockRoute.localRotation = Quaternion.Lerp(LockRoute.localRotation, _bodyRotaion, _rotationSpeed * Time.fixedDeltaTime);
+        //transform.forward = Vector3.Lerp(transform.forward, BodyJoint.forward, _rotationSpeed * Time.fixedDeltaTime);
+        var r = LegTrans.rotation;
+        LegBase.localRotation = LockRoute.localRotation;
+        LegTrans.rotation = r;
     }
 }
