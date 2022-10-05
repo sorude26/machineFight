c863cf2fd5504998ba36d376da52b1ed791273e1
sorude26 <sorude26@gmail.com>
2022-10-05 20:22:33 +0900

    ダメージ処理用インターフェースを追加した

---
+++ Assets/MyGame/Scripts/InGameSystem/IDamageApplicable.cs
@@ -0,0 +1,11 @@
+﻿/// <summary>
+/// ダメージ計算を行うインターフェース
+/// </summary>
+public interface IDamageApplicable
+{
+    /// <summary>
+    /// ダメージを与える
+    /// </summary>
+    /// <param name="damage"></param>
+    void AddlyDamage(int damage);
+}
