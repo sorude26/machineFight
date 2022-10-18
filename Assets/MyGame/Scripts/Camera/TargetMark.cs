6c5c126d016f9eac85264b202aa7a9923b7c9861
sorude26 <sorude26@gmail.com>
2022-10-18 20:31:25 +0900

    ターゲットマーカーのスクリプトを追加した

---
+++ Assets/MyGame/Scripts/Camera/TargetMark.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TargetMark : MonoBehaviour
+{
+    [SerializeField]
+    RectTransform _rect = default;
+    public Transform Target = default;
+    private bool _isActive = false;
+    private void FixedUpdate()
+    {
+        if (Target == null && _isActive == true)
+        {
+            _isActive = false;
+            _rect.gameObject.SetActive(_isActive);
+            return;
+        }       
+        else if (Target == null)
+        {
+            return;
+        }
+        else if (_isActive == false)
+        {
+            _isActive = true;
+            _rect.gameObject.SetActive(_isActive);
+        }
+        _rect.position = RectTransformUtility.WorldToScreenPoint(Camera.main, Target.position);
+    }
+}
