529595d2284fcd8ecca57b6e9ae2b7e631e7f2e6
sorude26 <sorude26@gmail.com>
2022-10-24 20:33:46 +0900

    武器の基底クラスを追加した

---
+++ Assets/MyGame/Scripts/Weapon/WeaponBase.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class WeaponBase : MonoBehaviour, IPartsModel
+{
+    [SerializeField]
+    private int _id = default;
+    [SerializeField]
+    protected int _power = 10;
+    [SerializeField]
+    protected float _speed = 200f;
+    [SerializeField]
+    protected ShakeParam _fireShakeParam = default;
+    public bool IsFire;
+    public int ID { get => _id; }
+    public float Speed { get => _speed; }
+    protected void PlayShake()
+    {
+        StageShakeController.PlayShake(transform.position + _fireShakeParam.Pos, _fireShakeParam.Power, _fireShakeParam.Time);
+    }
+    public abstract void Fire();
+}
