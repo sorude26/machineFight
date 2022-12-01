132b1c0c78bd7d025a74620c5b67f1e1063c38bb
sorude26 <sorude26@gmail.com>
2022-12-01 21:18:06 +0900

    誘導性能を外部から設定可能にした

--- Assets/MyGame/Scripts/Weapon/HomingBullet.cs
+++ Assets/MyGame/Scripts/Weapon/HomingBullet.cs
@@ -12,16 +12,21 @@ public class HomingBullet : ShotBullet
     private float _homingStartTime = 0f;
     [SerializeField]
     private float _homingEndTime = 5f;
+    [SerializeField]
+    private int _lockFrame = 0;
     private Transform _target = null;
     private float _homingTimer = 0;
+    private int _lockCount = 0;
     protected override void MoveBullet()
     {
         if (_speed <= 0) { return; }
-        if (_homingTimer < _homingEndTime && _homingTimer > _homingStartTime && _target != null && _target.gameObject.activeInHierarchy == true)
+        if (_lockCount >= _lockFrame && _homingTimer < _homingEndTime && _homingTimer > _homingStartTime && _target != null && _target.gameObject.activeInHierarchy == true)
         {
+            _lockCount = 0;
             _targetlocker.LookAt(_target);
             transform.forward = Vector3.Lerp(transform.forward, _targetlocker.forward, _homingSpeed * Time.fixedDeltaTime);
         }
+        _lockCount++;
         transform.position += transform.forward * _speed * Time.fixedDeltaTime;
         _timer -= Time.fixedDeltaTime;
         _homingTimer += Time.fixedDeltaTime;
@@ -39,6 +44,9 @@ public class HomingBullet : ShotBullet
         _exPower = param.ExplosionPower;
         _exCount = param.ExplosionCount;
         _exRadius = param.Radius;
+        _homingSpeed = param.HomingSpeed;
+        _homingStartTime = param.HomingStartTime;
+        _homingEndTime = param.HomingEndTime;
         _timer = _lifeTime;
         _homingTimer = 0;
         _frameCount = 0;
