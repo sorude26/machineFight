6c40cbdf1aa27f9fc4227903071101d0b4e9fd03
kojima0725 <kojikoji0725kt@gmail.com>
2022-12-05 19:10:42 +0900

    つまみ入力の修正

--- Assets/KojimaFolder/Scripts/OculusGameInput.cs
+++ Assets/KojimaFolder/Scripts/OculusGameInput.cs
@@ -1,16 +1,32 @@
+using Oculus.Interaction.Input;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+//メモ コントローラーを左右個別に扱った場合
+//上ボタン　Two
+//下ボタン  One
+//人差し指トリガー  PrimaryIndexTrigger
+//中指トリガー  PrimaryHandTrigger
+//サムスティック押し込み  Button.PrimaryThumbstick
+//サムスティック Axis2D.PrimaryThumbstick
+
+//左手スタートボタン Button.start
+//右手オキュラスボタン なし？
 
 public static class OculusGameInput
 {
     public static bool GetPinchIn(OVRInput.Controller controller)
     {
-        return OVRInput.GetDown(OVRInput.Button.Two, controller);
+        if (OVRInput.GetDown(OVRInput.Button.Two, controller) || OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, controller))
+        {
+            return OVRInput.Get(OVRInput.Button.Two, controller) && OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger, controller);
+        }
+        return false;
     }
     public static bool GetPinchOut(OVRInput.Controller controller)
     {
-        return !OVRInput.Get(OVRInput.Button.Two, controller);
+        return !OVRInput.Get(OVRInput.Button.Two, controller) || !OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger, controller);
     }
 
     public static bool GetGrabIn(OVRInput.Controller controller)
