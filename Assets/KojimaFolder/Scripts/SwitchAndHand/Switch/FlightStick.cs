82c2238f900b3c90494346bb33953ae6aa11eb10
kojima0725 <kojikoji0725kt@gmail.com>
2022-12-06 04:24:32 +0900

    スティックからトリガーとサムスティック入力を取れるようにした

--- Assets/KojimaFolder/Scripts/SwitchAndHand/Switch/FlightStick.cs
+++ Assets/KojimaFolder/Scripts/SwitchAndHand/Switch/FlightStick.cs
@@ -2,10 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// このクラスは基底クラスのONOFFの値を使わない、各操作部分の入力は個別の関数で取れる。
+/// </summary>
 public class FlightStick : Switch
 {
     [SerializeField]
     float _holdMoveOffsetX;
+
+    /// <summary>
+    /// フライトスティック、サムスティックの入力を取る
+    /// </summary>
+    /// <returns></returns>
+    public Vector2 GetThumbsStickInput()
+    {
+        if (_lockinHand == null) return Vector2.zero;
+        return OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, _lockinHand.ControllerType);
+    }
+
+    /// <summary>
+    /// フライトスティック、人差し指トリガーの入力を取る
+    /// </summary>
+    /// <param name="down"></param>
+    /// <returns></returns>
+    public bool GetTriggerInput(bool down = false)
+    {
+        if (_lockinHand == null) return false;
+        if (down)
+        {
+            return OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, _lockinHand.ControllerType);
+        }
+        else
+        {
+            return OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger, _lockinHand.ControllerType);
+        }
+    }
 
     protected override void LockIn(SwitchCtrlHand from)
     {
