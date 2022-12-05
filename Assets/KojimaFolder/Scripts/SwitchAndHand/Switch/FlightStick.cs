ebd3b3bbb2327f47e5dd5cc612b215e9e616c31d
kojima0725 <kojikoji0725kt@gmail.com>
2022-12-06 04:13:43 +0900

    掴み時のオフセット

--- Assets/KojimaFolder/Scripts/SwitchAndHand/Switch/FlightStick.cs
+++ Assets/KojimaFolder/Scripts/SwitchAndHand/Switch/FlightStick.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class FlightStick : Switch
 {
-    
+    [SerializeField]
+    float _holdMoveOffsetX;
+
+    protected override void LockIn(SwitchCtrlHand from)
+    {
+        float offset = _holdMoveOffsetX;
+        if (!from.IsRightHand)
+        {
+            offset = -offset;
+        }
+        var pos = _holdPosition.localPosition;
+        pos.x = offset;
+        _holdPosition.localPosition = pos;
+        base.LockIn(from);
+    }
 }
