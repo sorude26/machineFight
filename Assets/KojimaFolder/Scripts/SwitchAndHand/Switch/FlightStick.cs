808b0bd5e356abbae34b7717f1056bb43a4d8cd5
kojima0725 <kojikoji0725kt@gmail.com>
2022-12-07 18:32:27 +0900

    フライトスティックの傾き入力

--- Assets/KojimaFolder/Scripts/SwitchAndHand/Switch/FlightStick.cs
+++ Assets/KojimaFolder/Scripts/SwitchAndHand/Switch/FlightStick.cs
@@ -7,8 +7,23 @@ using UnityEngine;
 /// </summary>
 public class FlightStick : Switch
 {
+    const float STICK_ANGLE_MAX = 45f;
+
     [SerializeField]
     float _holdMoveOffsetX;
+    [SerializeField]
+    Transform _stickRotateBase;
+
+    Vector3 _currentStickRotate;
+
+    /// <summary>
+    /// フライトスティック、本体の入力を取る
+    /// </summary>
+    /// <returns></returns>
+    public Vector2 GetStickBodyInput()
+    {
+        return new Vector2(_currentStickRotate.x / STICK_ANGLE_MAX, _currentStickRotate.z / STICK_ANGLE_MAX);
+    }
 
     /// <summary>
     /// フライトスティック、サムスティックの入力を取る
@@ -49,5 +64,20 @@ public class FlightStick : Switch
         pos.x = offset;
         _holdPosition.localPosition = pos;
         base.LockIn(from);
+    }
+
+    protected override void SwitchUpdateOnLockIn()
+    {
+        base.SwitchUpdateOnLockIn();
+        Vector3 local = (Quaternion.Inverse(this.transform.rotation) * _lockinHand.transform.parent.rotation).eulerAngles.NomalizeRotate();
+        local.y = 0;
+        //回転がスティックの限界値を超えないように
+        local.x = Mathf.Min(local.x, STICK_ANGLE_MAX);
+        local.x = Mathf.Max(local.x, -STICK_ANGLE_MAX);
+        local.z = Mathf.Min(local.z, STICK_ANGLE_MAX);
+        local.z = Mathf.Max(local.z, -STICK_ANGLE_MAX);
+        //回転適用
+        _stickRotateBase.localRotation = Quaternion.Euler(local);
+        _currentStickRotate = local;
     }
 }
