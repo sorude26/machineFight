1d2a3ef51e9d7b0f8a0982cfd37a8879042388ce
HiraEi <aeblanket@gmail.com>
2022-12-07 13:56:35 +0900

    サウンドテストをしやすく変更した

--- Assets/MyGame/Scripts/Sound/SoundTester.cs
+++ Assets/MyGame/Scripts/Sound/SoundTester.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>サウンドテスター</summary>
+/// <summary>サウンドテスト用クラス</summary>
 public class SoundTester : MonoBehaviour
 {
     [SerializeField]
@@ -13,41 +13,69 @@ public class SoundTester : MonoBehaviour
     Material _defaultMaterial;
     [SerializeField]
     Material _activeMaterial;
-    [SerializeField]
     MeshRenderer _rightRenderer;
-    [SerializeField]
     MeshRenderer _leftRenderer;
+
+    [SerializeField]
+    bool _isPlayBGM = false;
+    [SerializeField]
+    int _playBGMIndex = 0;
+    [SerializeField]
+    int _playRightIndex = 0;
+    [SerializeField]
+    int _playLeftIndex = 0;
+    [SerializeField]
+    int _playCount = 1;
 
     void Start()
     {
+        _rightRenderer = _right.GetComponent<MeshRenderer>();
+        _rightRenderer = _left.GetComponent<MeshRenderer>();
+        
         StartCoroutine(Play());
     }
 
     IEnumerator Play()
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(2.0f);
 
-        SoundManager.Instance.PlayBGM(0, 0.5f);
-
+        if (_isPlayBGM)
+        {
+            Debug.Log(_playBGMIndex + "番のBGMを再生します");
+            SoundManager.Instance.PlayBGM(_playBGMIndex, 0.5f);
+        }
+        
         while (true)
         {
-            yield return new WaitForSeconds(3.0f);
+            yield return new WaitForSeconds(2.0f);
 
             //-------------可視化用-------------------//
             _rightRenderer.material = _activeMaterial;
             _leftRenderer.material = _defaultMaterial;
             //----------------------------------------//
 
-            SoundManager.Instance.Play3DSE(1, _right.transform.position);
+            Debug.Log(_playRightIndex + "番を" + _playCount + "回再生します");
+            StartCoroutine(TestPlay(_playRightIndex, _right.transform.position));
             
-            yield return new WaitForSeconds(3.0f);
+            yield return new WaitForSeconds(2.0f);
             
             //-------------可視化用-------------------//
             _rightRenderer.material = _defaultMaterial;
             _leftRenderer.material = _activeMaterial;
             //----------------------------------------//
 
-            SoundManager.Instance.Play3DSE(2, _left.transform.position);
+            Debug.Log(_playLeftIndex + "番を" + _playCount + "回再生します");
+            StartCoroutine(TestPlay(_playLeftIndex, _left.transform.position));
         }
+    }
+
+    IEnumerator TestPlay(int index, Vector3 pos)
+    {
+        for(int i = 0; i < _playCount; i++)
+        {
+            SoundManager.Instance.PlaySE(index, pos);
+            yield return new WaitForSeconds(0.2f);
+        }
+        yield break;
     }
 }
