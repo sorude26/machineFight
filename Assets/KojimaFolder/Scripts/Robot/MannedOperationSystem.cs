2e2553e82832774f14404d7c5fd2931af314f497
kojima0725 <kojikoji0725kt@gmail.com>
2023-01-22 21:01:11 +0900

    UIベース部分完成

--- Assets/KojimaFolder/Scripts/Robot/MannedOperationSystem.cs
+++ Assets/KojimaFolder/Scripts/Robot/MannedOperationSystem.cs
@@ -10,6 +10,7 @@ public class MannedOperationSystem : MonoBehaviour
 {
     const string CONNECTING_MESSAGE = "CONNECTING TO MAINSYSTEM...";
     const string CONNECTED_MESSAGE = "CONNECTED";
+    const string ONLINE_MESSAGE = "READY TO GO";
     const string CONNECTED_POPUP_MESSAGE = "拡張機能が接続されました";
 
     public static MannedOperationSystem Instance { get; private set; }
@@ -18,6 +19,10 @@ public class MannedOperationSystem : MonoBehaviour
     Switch _systemStartSwitch;
     [SerializeField]
     Text _connectText;
+    [SerializeField]
+    GameObject _mainCanvas;
+    [SerializeField]
+    GameObject _systemCanvas;
     [SerializeField]
     GameObject _monitorCover;
 
@@ -31,7 +36,11 @@ public class MannedOperationSystem : MonoBehaviour
     private void Awake()
     {
         Instance = this;
-        _systemStartSwitch.OnTurnOn += TrySystemStart;
+        if (_systemStartSwitch != null)
+        {
+            _systemStartSwitch.OnTurnOn += TrySystemStart;
+        }
+        _monitorCover.SetActive(true);
     }
 
     private void TrySystemStart()
@@ -41,12 +50,45 @@ public class MannedOperationSystem : MonoBehaviour
         //モニターがすべて開いていなければ起動しない
         if (!UIMonitorAnimator.Instance?.IsAllOpen ?? true) return;
         SystemStart();
-        _online = true;
     }
 
     private void SystemStart()
     {
+        _online = true;
+        StartCoroutine(SystemStartSequence());
+    }
+
+    IEnumerator SystemStartSequence()
+    {
+        //画面起動
         _connectText.text = "";
         _monitorCover.SetActive(false);
+        _systemCanvas.SetActive(true);
+        _mainCanvas.SetActive(false);
+        yield return new WaitForSeconds(1f);
+
+        //接続開始
+        _connectText.text = CONNECTING_MESSAGE;
+        yield return new WaitForSeconds(2.0f);
+
+        //接続完了
+        _connectText.text = CONNECTED_MESSAGE;
+        PopUpLog.CreatePopUp(CONNECTED_POPUP_MESSAGE);
+        //メイン画面のUI変更
+        ChangeMainUI();
+        yield return new WaitForSeconds(2f);
+
+        //操縦可能
+        _connectText.text = ONLINE_MESSAGE;
+        _connectedToDesctopUI = true;
+        yield return new WaitForSeconds(1f);
+
+        //全UI表示
+        _systemCanvas.SetActive(false);
+    }
+
+    private void ChangeMainUI()
+    {
+
     }
 }
