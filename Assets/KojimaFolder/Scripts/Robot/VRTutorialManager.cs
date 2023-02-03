4a4e61835e8c1d3510efb71959a7f0cf41da9b8e
kojima0725 <kojikoji0725kt@gmail.com>
2023-02-04 04:54:13 +0900

    チュートリアル機能作成中

---
+++ Assets/KojimaFolder/Scripts/Robot/VRTutorialManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRTutorialManager : MonoBehaviour
+{
+    public static VRTutorialManager Instance { get; private set; }
+    [SerializeField]
+    GameObject _iconPrefab;
+
+    private Dictionary<Transform, GameObject> icons = new Dictionary<Transform, GameObject>();
+
+    /// <summary>
+    /// メインモニタ、レーダーを開けてチュートリアルを表示できるようにする
+    /// </summary>
+    public void TutorialSetup()
+    {
+
+    }
+
+    /// <summary>
+    /// 操作してほしい部分にアイコンを出現させる
+    /// </summary>
+    public void MakeIcon(Transform pos)
+    {
+        var icon = Instantiate(_iconPrefab);
+        icon.transform.position = pos.position;
+        icon.transform.rotation = Quaternion.identity;
+        icon.transform.parent = pos;
+        icons.Add(pos, icon);
+    }
+
+    /// <summary>
+    /// アイコンを削除する
+    /// </summary>
+    public void DeleteIcon(Transform pos)
+    {
+        if (icons.ContainsKey(pos))
+        {
+            Destroy(icons[pos]);
+            icons.Remove(pos);
+        }
+    }
+}
